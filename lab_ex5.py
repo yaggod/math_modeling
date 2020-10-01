0a17a9ea5213f637910587d690cfcc191c833441
yaggod <71946695+yaggod@users.noreply.github.com>
2020-10-01 18:07:46 +0200

    Делимость

---
+++ lab_ex5.py
@@ -0,0 +1,14 @@
+print("Введите делимое")
+a=int(input())
+print("Введите делитель")
+b=int(input())
+if b==0:
+    print("Делитель равен 0")
+    exit()
+if a%b==0:
+    print("Делится нацело")
+else:
+    aaaa=str("Не делится нацело, остаток равен")
+    print(aaaa, a%b)
+aaaa=str("Частное равно")
+print(aaaa, int(a/b))
