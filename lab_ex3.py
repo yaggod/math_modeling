0e5c602478df2c8694602566d4f4f665671cca25
yaggod <71946695+yaggod@users.noreply.github.com>
2020-10-29 18:11:44 +0200

    Delete lab_ex3.py

--- lab_ex3.py
+++
@@ -1,7 +0,0 @@
-from const import g
-def MechEn(m,h,v):
-    return ((m*(v**2))/2)+m*h*g
-m=int(input("Масса тела (кг) "))
-h=int(input("Высота полета (м) "))
-v=int(input("Скорость тела  (м/c) "))
-print(MechEn(m,h,v))
